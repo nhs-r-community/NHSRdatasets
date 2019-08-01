4437232b1b28ce5f9d138aaebef15aa4e63a2dcb
Chris Mainey <39626211+chrismainey@users.noreply.github.com>
2019-08-01 23:35:17 +0100

    Update test-LOS_model.R

--- tests/testthat/test-LOS_model.R
+++ tests/testthat/test-LOS_model.R
@@ -1,4 +1,4 @@
-test_that("dataset is as expectes", {
+test_that("dataset is as expected", {
   expect_type(LOS_model, "list")
   expect_s3_class(LOS_model$Organisation, "factor")
   expect_s3_class(LOS_model, "data.frame")
