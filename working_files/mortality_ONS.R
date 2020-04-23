0929427c05b9368736ce92763ee4eca6fd688369
Turner Zoe - Senior Information Analyst <Zoe.Turner2@nottshc.nhs.uk>
2020-04-23 16:50:54 +0100

    Corrected the category labelling. Changed the output file name to ons_mortality.rda. Removed 2020 import and formatting - this will be available in a separate script.

--- working_files/mortality_ONS.R
+++ working_files/mortality_ONS.R
@@ -14,14 +14,6 @@ library(lubridate)
 
 # Download data -----------------------------------------------------------
 
-# 2020 Format changed to xlsx from xls
-
-download.file(
-  "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fbirthsdeathsandmarriages%2fdeaths%2fdatasets%2fweeklyprovisionalfiguresondeathsregisteredinenglandandwales%2f2020/referencetablesweek142020.xlsx",
-  destfile = "2020Mortality.xlsx",
-  method = "wininet",
-  mode = "wb")
-
 # 2019
 download.file(
   "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fbirthsdeathsandmarriages%2fdeaths%2fdatasets%2fweeklyprovisionalfiguresondeathsregisteredinenglandandwales%2f2019/publishedweek522019.xls",
@@ -164,35 +156,40 @@ for(i in files_list_sheets) {
   assign(i, x)
 }
 
-# Format data 2010 - 2016 -----------------------------------------------------------
+
+
+# Repeated code -----------------------------------------------------------
+
+remove_lookup <- c('week over the previous five years1',
+  'Deaths by underlying cause2,3',
+  'Footnotes',
+  '1 This average is based on the actual number of death registrations recorded for each corresponding week over the previous five years. Moveable public holidays, when register offices are closed, affect the number of registrations made in the published weeks and in the corresponding weeks in previous years.',
+  '2 Counts of deaths by underlying cause exclude deaths at age under 28 days.',
+  '3 Coding of deaths by underlying cause for the latest week is not yet complete.',
+  "4Does not include deaths where age is either missing or not yet fully coded. For this reason counts of 'Persons', 'Males' and 'Females' may not sum to 'Total Deaths, all ages'.",
+  '5 Does not include deaths of those resident outside England and Wales or those records where the place of residence is either missing or not yet fully coded. For this reason counts for "Deaths by Region of usual residence" may not sum to "Total deaths, all ages".',
+  'Source: Office for National Statistics',
+  'Deaths by age group'
+)
+
+# Format data 2010 - 2015 -----------------------------------------------------------
 
 formatFunction <- function(file){
 
 ONS <- file %>%
   clean_names %>%
   remove_empty(c("rows","cols")) %>%
-  filter(!contents %in% c('week over the previous five years1',
-                          'Deaths by underlying cause2,3',
-                          'Footnotes',
-                          '1 This average is based on the actual number of death registrations recorded for each corresponding week over the previous five years. Moveable public holidays, when register offices are closed, affect the number of registrations made in the published weeks and in the corresponding weeks in previous years.',
-                          '2 Counts of deaths by underlying cause exclude deaths at age under 28 days.',
-                          '3 Coding of deaths by underlying cause for the latest week is not yet complete.',
-                          "4Does not include deaths where age is either missing or not yet fully coded. For this reason counts of 'Persons', 'Males' and 'Females' may not sum to 'Total Deaths, all ages'.",
-                          '5 Does not include deaths of those resident outside England and Wales or those records where the place of residence is either missing or not yet fully coded. For this reason counts for "Deaths by Region of usual residence" may not sum to "Total deaths, all ages".',
-                          'Source: Office for National Statistics',
-                          'Deaths by age group'
-  )) %>%
-  mutate(Category = case_when(is.na(x2) & str_detect(contents, " 4") ~ str_replace(contents, " 4", ""),
-                              is.na(x2) & str_detect(contents, "Region") ~ "Region",
+  filter(!contents %in% remove_lookup) %>%
+  mutate(Category = case_when(is.na(x2) & str_detect(contents, "Region") ~ "Region",
                               is.na(x2) & str_detect(contents, "Persons") ~ "Persons",
                               is.na(x2) & str_detect(contents, "Females") ~ "Females",
                               is.na(x2) & str_detect(contents, "Males") ~ "Males")
   ) %>%
   select(contents, Category, everything()) %>%
   fill(Category) %>%
-  filter(!str_detect(contents, 'Persons'),
-         !str_detect(contents, 'Males'),
-         !str_detect(contents, 'Females')) %>%
+  filter(!str_detect(contents, "Persons"),
+         !str_detect(contents, "Males"),
+         !str_detect(contents, "Females")) %>%
   unite("Categories", Category, contents) %>%
   filter(Categories != 'Region_Deaths by Region of usual residence 5') %>%
   mutate(Categories = case_when(str_detect(Categories, "NA_") ~ str_replace(Categories, "NA_", ""),
@@ -201,6 +198,8 @@ ONS <- file %>%
 # Push date row to column names
 
 onsFormattedJanitor <- row_to_names(ONS, 3)
+
+# Move to long form and further formatting
 
 x <- onsFormattedJanitor %>%
   pivot_longer(cols = -`Week ended`,
@@ -239,8 +238,9 @@ x <- onsFormattedJanitor %>%
          week_no
          ) %>%
   filter(!is.na(counts),
-         counts != ":") %>%
+                  counts != ":") %>%
   fill(category_1)
+
 
 return(x)
 
@@ -248,7 +248,7 @@ return(x)
 
 Mortality2010 <- formatFunction(`Working files/Weekly/2010Mortality-Weekly Figures 2010.csv`)
 Mortality2011 <- formatFunction(`Working files/Weekly/2011Mortality-Weekly Figures 2011.csv`) %>%
-  mutate(category_2 = case_when(category_1 == "All respiratory diseases (ICD-10 J00-J99) ICD-10" ~ "v 2010",
+  mutate(category_2 = case_when(is.na(category_2) & category_1 == "All respiratory diseases (ICD-10 J00-J99) ICD-10" ~ "v 2010",
                                 TRUE ~ category_2))
 
 Mortality2012 <- formatFunction(`Working files/Weekly/2012Mortality-Weekly Figures 2012.csv`)
@@ -258,9 +258,8 @@ Mortality2015 <- formatFunction(`Working files/Weekly/2015Mortality-Weekly Figur
 
 
 # Format data 2016 - 2019 -------------------------------------------------
-# Format data 2020 - (added skip line)
 
-formatFunction2016 <- function(file, skip){
+formatFunction2016 <- function(file){
 
   ONS <- file %>%
     clean_names %>%
@@ -268,25 +267,18 @@ formatFunction2016 <- function(file, skip){
                               TRUE ~ x2)) %>%
     remove_empty(c("rows","cols")) %>%
     select(-contents) %>%
-    filter(!x2 %in% c('week over the previous five years1',
-                            'Deaths by underlying cause2,3',
-                            'Footnotes',
-                            '1 This average is based on the actual number of death registrations recorded for each corresponding week over the previous five years. Moveable public holidays, when register offices are closed, affect the number of registrations made in the published weeks and in the corresponding weeks in previous years.',
-                            '2 Counts of deaths by underlying cause exclude deaths at age under 28 days.',
-                            '3 Coding of deaths by underlying cause for the latest week is not yet complete.',
-                            "4Does not include deaths where age is either missing or not yet fully coded. For this reason counts of 'Persons', 'Males' and 'Females' may not sum to 'Total Deaths, all ages'.",
-                            '5 Does not include deaths of those resident outside England and Wales or those records where the place of residence is either missing or not yet fully coded. For this reason counts for "Deaths by Region of usual residence" may not sum to "Total deaths, all ages".',
-                            'Source: Office for National Statistics',
-                            'Deaths by age group'
-    )) %>%
-    mutate(Category = case_when(is.na(x3) & str_detect(x2, " 4") ~ str_replace(x2, " 4", ""),
-                                is.na(x3) & str_detect(x2, " 5") ~ "Region")
+    filter(!x2 %in% remove_lookup) %>%
+    mutate(Category = case_when(is.na(x3) & str_detect(x2, "region") ~ "Region",
+                                is.na(x3) & str_detect(x2, "Persons") ~ "Persons",
+                                is.na(x3) & str_detect(x2, "Females") ~ "Females",
+                                is.na(x3) & str_detect(x2, "Males") ~ "Males",
+                                TRUE ~ NA_character_)
     ) %>%
     select(x2, Category, everything()) %>%
     fill(Category) %>%
-    filter(!x2 %in% c('Persons 4',
-                            'Males 4',
-                            'Females 4')) %>%
+    filter(!str_detect(x2, "Persons"),
+           !str_detect(x2, "Males"),
+           !str_detect(x2, "Females")) %>%
     unite("Categories", Category, x2) %>%
     filter(Categories != 'Region_Deaths by Region of usual residence 5') %>%
     mutate(Categories = case_when(str_detect(Categories, "NA_") ~ str_replace(Categories, "NA_", ""),
@@ -294,40 +286,63 @@ formatFunction2016 <- function(file, skip){
 
   # Push date row to column names
 
-  onsFormattedJanitor <- row_to_names(ONS, skip)
+  onsFormattedJanitor <- row_to_names(ONS, 3)
+
+  # Move to long form and further formatting
 
   x <- onsFormattedJanitor %>%
     pivot_longer(cols = -`Week ended`,
                  names_to = "allDates",
-                 values_to = "Counts") %>%
+                 values_to = "counts") %>%
     mutate(realDate = dmy(allDates),
            ExcelSerialDate = case_when(stri_length(allDates) == 5 ~ excel_numeric_to_date(as.numeric(allDates), date_system = "modern")),
-           Date = case_when(is.na(realDate) ~ ExcelSerialDate,
+           date = case_when(is.na(realDate) ~ ExcelSerialDate,
                             TRUE ~ realDate)) %>%
     group_by(`Week ended`) %>%
-    mutate(WeekNo = row_number()) %>%
+    mutate(week_no = row_number()) %>%
     ungroup() %>%
-    rename(Category = `Week ended`)
+    rename(Category = `Week ended`) %>%
+    mutate(category_1 = case_when(str_detect(Category, ",") ~
+                                  substr(Category,1,str_locate(Category, ",") -1),
+                                str_detect(Category, ":") ~
+                                  substr(Category,1,str_locate(Category, ":") -1),
+                                str_detect(Category, "_") ~
+                                  substr(Category,1,str_locate(Category, "_") -1),
+                                str_detect(Category, "respiratory")  ~
+                                  "All respiratory diseases (ICD-10 J00-J99) ICD-10"),
+         category_2 = case_when(str_detect(Category, ",") ~
+                                  substr(Category,str_locate(Category, ", ") +2, str_length(Category)),
+                                str_detect(Category, ":") ~
+                                  substr(Category,str_locate(Category, ": ") +2, str_length(Category)),
+                                str_detect(Category, "_") ~
+                                  substr(Category,str_locate(Category, "_") +1, str_length(Category)),
+                                str_detect(Category, "respiratory")  ~
+                                  substr(Category,str_locate(Category, "v"), str_length(Category)) ),
+         category_2 = recode(category_2,
+                             "average of corresponding" = "average of same week over 5 years")) %>%
+    select(category_1,
+           category_2,
+           counts,
+           date,
+           week_no
+    ) %>%
+    filter(!is.na(counts))
 
   return(x)
 
 }
 
 
-Mortality2016 <- formatFunction2016(`Working files/Weekly/2016Mortality-Weekly figures 2016.csv`, 3)
-Mortality2017 <- formatFunction2016(`Working files/Weekly/2017Mortality-Weekly figures 2017.csv`, 3)
-Mortality2018 <- formatFunction2016(`Working files/Weekly/2018Mortality-Weekly figures 2018.csv`, 3)
-Mortality2019 <- formatFunction2016(`Working files/Weekly/2019Mortality-Weekly figures 2019.csv`, 3)
-
-
-
-
-Mortality2020 <- formatFunction2016(`Working files/Weekly/2020Mortality-Weekly figures 2020.csv`, 4)
+Mortality2016 <- formatFunction2016(`Working files/Weekly/2016Mortality-Weekly figures 2016.csv`)
+Mortality2017 <- formatFunction2016(`Working files/Weekly/2017Mortality-Weekly figures 2017.csv`)
+Mortality2018 <- formatFunction2016(`Working files/Weekly/2018Mortality-Weekly figures 2018.csv`)
+Mortality2019 <- formatFunction2016(`Working files/Weekly/2019Mortality-Weekly figures 2019.csv`)
 
 
 # Bind together -----------------------------------------------------------
 
-Mortality <- do.call("rbind", list(Mortality2010,
+ons_mortality <- do.call("rbind", list(
+                      Mortality2010,
                       Mortality2011,
                       Mortality2012,
                       Mortality2013,
@@ -336,12 +351,8 @@ Mortality <- do.call("rbind", list(Mortality2010,
                       Mortality2016,
                       Mortality2017,
                       Mortality2018,
-                      Mortality2019)) %>%
-  select(-allDates,
-         -realDate,
-         -ExcelSerialDate)
-
+                      Mortality2019))
 
   # Save as RData file
 
-  save(Mortality, file = "data/ONSMortality.RData")
+  save(ons_mortality, file = "data/ons_mortality.rda")
