a0f0a1a101a386fe8d64fc43c857526bcc4b1614
Fran Barton <francis.barton@nuh.nhs.uk>
2024-01-25 15:16:22 +0000

    Begin data import process for APHA Analysts dataset

--- data-raw/apha_analysts.R
+++ data-raw/apha_analysts.R
@@ -1,6 +1,13 @@
 ## code to prepare `apha_analysts` dataset goes here
 
 
+# https://www.aphanalysts.org/documents/cpd-survey-results-raw-data/
 
+raw_data_url <- paste0(
+  "https://www.aphanalysts.org/",
+  "document/get/file/CPD-Survey-Results-RAW-Data.xlsx"
+  )
+
+raw_data <- openxlsx2::read_xlsx(raw_data_url)
 
 usethis::use_data(apha_analysts, overwrite = TRUE)
