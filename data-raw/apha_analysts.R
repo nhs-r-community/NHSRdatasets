7899a8eb25b78809393bd3b019e0640ea992dfc5
Fran Barton <francis.barton@nuh.nhs.uk>
2024-01-25 21:39:27 +0000

    Add httr2 pipelines to download xlsx data file

--- data-raw/apha_analysts.R
+++ data-raw/apha_analysts.R
@@ -1,13 +1,40 @@
-## code to prepare `apha_analysts` dataset goes here
+# Source: https://www.aphanalysts.org/documents/cpd-survey-results-raw-data/
+# (Accessed 25 January 2024)
+# 
+# The .xlsx file cannot be directly downloaded from its URI as there is
+# protection on the file: it must be downloaded by submitting the form on the
+# web page, either manually by clicking the button on the page, or by
+# simulating the form submission using form headers within an {httr2} pipeline.
 
-
-# https://www.aphanalysts.org/documents/cpd-survey-results-raw-data/
+# HTML form code, copied from above URL, and used as source for form headers:
+# 
+# <form method="post" class="CMDM-downloadForm" action="https://www.aphanalysts.org/document/get/file/CPD-Survey-Results-RAW-Data.xlsx">
+# 	<input type="hidden" name="cmdm_nonce" value="c133038289">
+# 	<input type="hidden" name="id" value="14354">
+# 	<input type="hidden" name="shortcodeId" value="31fae3b9363d47608d9038cffc3fa471">
+# 	<input type="hidden" name="backurl" value="/documents/cpd-survey-results-raw-data/#cmdm-shortcode-31fae3b9363d47608d9038cffc3fa471">
+# 	<div class="field">
+# 	  <a href="#" class="downloadBut cmdm-download-button cmdm-button cmdm-button-primary cmdm-button-large">Download</a>
+# 	  <input type="submit" value="Download" style="display:none">
+# 	</div>
+# </form>
 
 raw_data_url <- paste0(
   "https://www.aphanalysts.org/",
   "document/get/file/CPD-Survey-Results-RAW-Data.xlsx"
   )
 
-raw_data <- openxlsx2::read_xlsx(raw_data_url)
+dl_tmp <- tempfile("apha_raw_data", fileext = ".xlsx")
 
-usethis::use_data(apha_analysts, overwrite = TRUE)
+httr2::request(raw_data_url) |>
+  httr2::req_body_form(
+    # These three headers are all required; the 4th one above isn't needed
+    cmdm_nonce = "c133038289",
+    id = "14354",
+    shortcodeId = "31fae3b9363d47608d9038cffc3fa471"
+    ) |>
+  httr2::req_perform() |>
+  # The lines below could be omitted and the `path` argument to req_perform()
+  # used instead; but the more verbose/explicit method seems clearer.
+  httr2::resp_body_raw() |>
+  readr::write_file(dl_tmp)
